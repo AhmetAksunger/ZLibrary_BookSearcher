b48a81d5f85ba95c2ecafcbd8b8377bf9107f108
Ahmet <ahmetaksungerbilgi@gmail.com>
2023-03-26 14:50:30 +0300

    Updated the script with WebDriverWait modules instead of sleep, now it's much faster

--- book_searcher.py
+++ book_searcher.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #Enter your zlibrary email and password
 email = ''
@@ -14,16 +15,22 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome(options=chrome_options)
 
+def wait_until(locator: tuple):
+    WebDriverWait(driver,10).until(EC.visibility_of_element_located(locator))
+
 def get_webpage(url):
 
     driver.get(url)
 
-    time.sleep(2)
-
 def get_login_fields():
-    email_field = driver.find_element('name','email')
 
-    password_field = driver.find_element('name','password')
+    wait_until((By.NAME,'email'))
+
+    email_field = driver.find_element(By.NAME,'email')
+
+    wait_until((By.NAME,'email'))
+
+    password_field = driver.find_element(By.NAME,'password')
 
     return email_field, password_field
 
@@ -33,15 +40,17 @@ def send_login_keys(email_field,password_field,email,password):
 
     password_field.send_keys(Keys.RETURN)
 
+    WebDriverWait(driver,10).until(EC.url_changes(url))
+
 def main_login(url,email,password):
     get_webpage(url)
     email_field,password_field = get_login_fields()
     send_login_keys(email_field,password_field,email,password)
-    time.sleep(3)
 
 def get_search_field():
 
-    search_field = driver.find_elements('name','q')
+    wait_until((By.XPATH,'//*[@id="searchFieldx"]'))
+    search_field = driver.find_elements(By.NAME,'q')
     return search_field
 
 def send_search_keys(search_field,search_input):
@@ -50,6 +59,8 @@ def send_search_keys(search_field,search_input):
     search_field[1].send_keys(Keys.RETURN)
 
 def get_books():
+
+    wait_until((By.XPATH,'//h3[@itemprop="name"]/a'))
     book_titles = driver.find_elements(By.XPATH,'//h3[@itemprop="name"]/a')
     count = 0
     books = {}
@@ -67,7 +78,6 @@ def main_search():
     while True:
         search_field = get_search_field()
         send_search_keys(search_field,search_input)
-        time.sleep(2)
         books,links = get_books()
         while True:
             index = input("(s: search another book, q: quit)\nEnter a book index: ")
